45594cbd1a68b61e716fc4a9c9033a42208bc162
Goran <goranaviani@gmail.com>
2018-02-08 17:34:38 +0100

    8 februari 2018

--- coinwallet.py
+++ coinwallet.py
@@ -1,7 +1,11 @@
 import os
 import json
 import time
+#version 1 , version 2 and vesion 3 bellow:
+coin_wallet={'XLM':42,'MIOTA':30,'COSS':50,'APPC':12}
 
+
+#version 1
 class CoinMarketCap():
     def __init__(self,):
         print("")
@@ -43,11 +47,11 @@ class CoinMarketCap():
             print("According to Coinmarketcap.com your cryptocurrency wallet is worth: "+ "%.2f" % round(wallet_sum,2) )
 
 
-coin_wallet={'XLM':42,'MIOTA':30,'COSS':50,'APPC':12}
-firsttask = CoinMarketCap()
-firsttask.get_price_lists()
+
+#firsttask = CoinMarketCap()
+#firsttask.get_price_lists()
 #firsttask.read_all_prices()
-firsttask.read_prices_for_my_wallet(coin_wallet)
+#firsttask.read_prices_for_my_wallet(coin_wallet)
 
 #####################################
 #2nd version- refresh every 60 seconds.
@@ -70,5 +74,33 @@ def read_prices_for_my_wallet(coin_wallet):
 
     time.sleep(5)
 
+#while True:
+#    read_prices_for_my_wallet(coin_wallet)
+
+################
+#version 3 - without txt file
+
+class CryptoProtfolioValue():
+    def __init__(self, prices_json,):
+        self.prices_json=prices_json
+
+    def read_prices(self,):
+        prices = os.popen("curl -X GET https://api.coinmarketcap.com/v1/ticker/?limit=0").read()
+        self.prices_json = json.loads(prices)
+        time.sleep(5)
+    def print_my_portfolio_value(self,):
+        wallet_sum=0
+        for x in coin_wallet:
+            for valuta in self.prices_json:
+                if valuta['symbol'] == x:
+                    print("*Currency: "+valuta['symbol']+ " has current price of: " + valuta['price_usd'])
+                    vrijednost = (float(valuta['price_usd']) * float(coin_wallet[x]))
+                    wallet_sum += vrijednost
+                    print("**Currency: "+valuta['symbol']+ " has a total value of: " + str(vrijednost))
+        print("According to Coinmarketcap.com your cryptocurrency wallet is worth: "+ "%.2f" % round(wallet_sum,2) )
+
+
+secondtask = CryptoProtfolioValue("")
 while True:
-    read_prices_for_my_wallet(coin_wallet)
+    secondtask.read_prices()
+    secondtask.print_my_portfolio_value()
